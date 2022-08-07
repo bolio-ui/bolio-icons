3da1ef766299426820ea20bd469f10d4b24ab81e
Bruno Andrade <brunnoandradi@gmail.com>
2022-08-07 16:57:29 -0300

    update rollup configs and add generate typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,43 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
 import filesize from 'rollup-plugin-filesize';
 import svgr from '@svgr/rollup';
+
 const packageJson = require('./package.json');
+
+const globals = {
+  ...packageJson.devDependencies,
+};
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      dir: packageJson.main,
-      format: 'esm',
+      file: packageJson.main,
+      format: 'cjs', // commonJS
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm', // ES Modules
       sourcemap: true,
     },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
-    typescript({ useTsconfigDeclarationDir: true }),
     commonjs(),
+    typescript({
+      useTsconfigDeclarationDir: true,
+    }),
+    commonjs({
+      exclude: 'node_modules',
+      ignoreGlobal: true,
+    }),
     svgr(),
     filesize(),
   ],
+  external: Object.keys(globals),
 };
